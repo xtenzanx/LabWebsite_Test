49ade3fb6e837b8d396cc4ef7192de640b0b698e
106598006 <xtenzanxt1@gmail.com>
2018-12-03 15:10:11 +0800

    嘗試加入Git(待完成)

--- groupmeeting_set.php
+++ groupmeeting_set.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type:text/html;charset=utf-8');
 
+require_once('Git.php');
+
 require_once("JSONDB.php");
 use Jajo\JSONDB;
 $json_db = new JSONDB( __DIR__ );
@@ -15,7 +17,28 @@ if( isset($_POST['gm_date']) && isset($_POST['gm_name']) && isset($_POST['gm_top
         ]
     );
 
+    sleep(1);
+
+    echo Git::get_bin();
+
+    echo '-----';
+    echo __DIR__;
+
+    $repo = Git::open( __DIR__ );  // -or- Git::create('/path/to/repo')
+
+    $repo->add('.');
+    // $repo->commit('GroupMeeting data update');
+    // $repo->push('origin', 'master');
+
+    // system('git add data/presentationHistory.json', $out);
+    // var_dump($out);
+    // print_r($out);
+    // exec('git commit -m "GroupMeeting data update"', $out, $r);
+    // var_dump($out);
+    // echo $r;
+    // exec("git push", $out);
+    // print_r('git push => '.$out);
 }
 
-header ("Location:  index.php");
+// header ("Location:  index.php");
 ?>
