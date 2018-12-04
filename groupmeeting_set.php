f5e1d00b255d5a618b2249541984d435e36e3253
林照晟 <xtenzanxt1@gmail.com>
2018-12-04 13:49:52 +0800

    GroupMeeting data update

--- groupmeeting_set.php
+++ groupmeeting_set.php
@@ -3,31 +3,26 @@ header('Content-Type:text/html;charset=utf-8');
 
 require_once('Git.php');
 
-require_once("JSONDB.php");
-use Jajo\JSONDB;
-$json_db = new JSONDB( __DIR__ );
+// require_once("JSONDB.php");
+// use Jajo\JSONDB;
+// $json_db = new JSONDB( __DIR__ );
 
 if( isset($_POST['gm_date']) && isset($_POST['gm_name']) && isset($_POST['gm_topic']) ){
 
-    $json_db->insert( 'data/presentationHistory.json', 
-        [ 
-            'date' => $_POST['gm_date'], 
-            'name' => $_POST['gm_name'], 
-            'topic' => $_POST['gm_topic']
-        ]
-    );
+    // $json_db->insert( 'data/presentationHistory.json', 
+    //     [ 
+    //         'date' => $_POST['gm_date'], 
+    //         'name' => $_POST['gm_name'], 
+    //         'topic' => $_POST['gm_topic']
+    //     ]
+    // );
 
-    sleep(1);
-
-    echo Git::get_bin();
-
-    echo '-----';
-    echo __DIR__;
+    // sleep(1);
 
     $repo = Git::open( __DIR__ );  // -or- Git::create('/path/to/repo')
 
-    $repo->add('.');
-    // $repo->commit('GroupMeeting data update');
+    $repo->add('data/presentationHistory.json');
+    $repo->commit('GroupMeeting data update');
     // $repo->push('origin', 'master');
 
     // system('git add data/presentationHistory.json', $out);
