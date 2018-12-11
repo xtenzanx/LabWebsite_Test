5efbe01a6c9dc4eeac6f2a00c0b3d625f722941e
SELab <labsesdt@gmail.com>
2018-12-11 11:51:49 +0800

    GroupMeeting data update

--- groupmeeting_set.php
+++ groupmeeting_set.php
@@ -9,14 +9,14 @@ if( isset($_POST['gm_date']) && isset($_POST['gm_name']) && isset($_POST['gm_top
     $json_db = new JsonTable(__DIR__."/data/presentationHistory.json", true);
     $json_db->insert(array('date' => $_POST['gm_date'], 'name' => $_POST['gm_name'], 'topic' => $_POST['gm_topic']));
 
-    sleep(0.3);
+    sleep(0.5);
 
     $repo = Git::open( __DIR__ );  // -or- Git::create('/path/to/repo')
     $repo->add('data/presentationHistory.json');
     $repo->commit('GroupMeeting data update');
-    $repo->run('config user.email "labsesdt@gmail.com"'); 
-    $repo->run('config user.name "SELab"');
-    $repo->run('config user.password "password"');
+    // $repo->run('config user.email "labsesdt@gmail.com"'); 
+    // $repo->run('config user.name "SELab"');
+    // $repo->run('config user.password "password"');
     $repo->push('origin', 'master');
 }
 
